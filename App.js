6d7ebfc773a805c031dfdacd963d3f8c6b8b9516
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-22 11:34:29 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello Brasil</Text>
+      </View>
+    );
+  }
+}

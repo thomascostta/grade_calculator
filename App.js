d28d306db1c21650a89f5de64c7bb7cfaedc66f2
thomascostta <thomas.jeffcosta@gmail.com>
2022-04-22 16:28:04 -0300

    update App.js

--- App.js
+++ App.js
@@ -1,15 +1,70 @@
 import React, {Component} from 'react';
 import {
   Text,
+  TouchableOpacity,
   View,
+  TextInput,
+  StyleSheet,
 } from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello Brasil</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Nota 1"
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Nota 2"
+        />
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Número de faltas"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textButton}>Calcular</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.textResult}>Valor foi aprovado</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    padding: 30,
+    backgroundColor: '#4287f5f5',
+    marginLeft: 50,
+    marginRight: 50,
+    marginTop: 30,
+    fontSize: 20,
+  },
+  button: {
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#37474f4f',
+  },
+  textButton: {
+    color: '#ffffff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontFamily: 'Arial',
+    alignSelf: 'center',
+  },
+  textResult: {
+    color: '#000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
